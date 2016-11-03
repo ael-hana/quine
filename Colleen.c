ad2a09e1ec30a7bd1939320a7deb0f451ccd6609
Anasse EL HANANI <ael-hana@student.42.fr>
2016-11-03 22:57:19 +0100

    premier quine 42

---
+++ Colleen.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void foo(){}
+//merci free
+int main(){/*vive 42*/char*c="#include <stdio.h>%cvoid foo(){}%c//merci free%cint main(){/*vive 42*/char*c=%c%s%c;printf(c,10,10,10,34,c,34,10);return(0);}%c";printf(c,10,10,10,34,c,34,10);return(0);}
