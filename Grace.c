9e0c932304dcf7673987ff1437f27d6caa8d3f38
Anasse EL HANANI <ael-hana@student.42.fr>
2016-11-04 22:38:10 +0100

    commit grace fix

--- Grace.c
+++ Grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-//commentaire inutile
+/*
+	commentaire inutile
+*/
 #define Nintendo
 #define VS
-#define SONY(x) int main(void){FILE*p=fopen("Grace_kid.c", "w");char*c="#include <stdio.h>%c//commentaire inutile%c#define Nintendo%c#define VS%c#define SONY(x) int main(void){FILE*p=fopen(%cGrace_kid.c%c, %cw%c);char*c=%c%s%c;fprintf(p,c,10,10,10,10,34,34,34,34,34,c,34,10,10);}%cSONY(42)%c";fprintf(p,c,10,10,10,10,34,34,34,34,34,c,34,10,10);}
-SONY(42)
+#define FT(x) int main(void){FILE*p=fopen("Grace_kid.c", "w");char*c="#include <stdio.h>%c/*%c%ccommentaire inutile%c*/%c#define Nintendo%c#define VS%c#define FT(x) int main(void){FILE*p=fopen(%cGrace_kid.c%c, %cw%c);char*c=%c%s%c;fprintf(p,c,10,10,9,10,10,10,10,34,34,34,34,34,c,34,10,10);}%cFT(42)%c";fprintf(p,c,10,10,9,10,10,10,10,34,34,34,34,34,c,34,10,10);}
+FT(42)
