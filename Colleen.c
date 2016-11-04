338a51f2f93b9233a4b7e8297e84fc7373855e2c
Anasse EL HANANI <ael-hana@student.42.fr>
2016-11-04 22:28:12 +0100

    Verif Colleen.c

--- Colleen.c
+++ Colleen.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 void foo(){}
-//merci free
-int main(){/*vive 42*/char*c="#include <stdio.h>%cvoid foo(){}%c//merci free%cint main(){/*vive 42*/char*c=%c%s%c;printf(c,10,10,10,34,c,34,10);return(0);}%c";printf(c,10,10,10,34,c,34,10);return(0);}
+/*
+	merci free
+*/
+int main(){
+/*
+	vive 42
+*/
+char*c="#include <stdio.h>%cvoid foo(){}%c/*%c%cmerci free%c*/%cint main(){%c/*%c%cvive 42%c*/%cchar*c=%c%s%c;printf(c,10,10,10,9,10,10,10,10,9,10,10,34,c,34,10);return(0);}%c";printf(c,10,10,10,9,10,10,10,10,9,10,10,34,c,34,10);return(0);}
