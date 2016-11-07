adc23d1e1cd906640a4c26e546028b8bf4821d17
Anasse EL HANANI <ael-hana@student.42.fr>
2016-11-07 12:28:14 +0100

    add bonus

---
+++ Colleen.php
@@ -0,0 +1,10 @@
+<?php
+function foo(){}
+/*
+	merci free
+*/
+function main(){
+/*
+	vive 42
+*/
+$c='<?php%cfunction foo(){}%c/*%c%cmerci free%c*/%cfunction main(){%c/*%c%cvive 42%c*/%c$c=%c%s%c;printf($c,10,10,10,9,10,10,10,10,9,10,10,39,$c,39,10);}main();%c';printf($c,10,10,10,9,10,10,10,10,9,10,10,39,$c,39,10);}main();
