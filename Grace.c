72816c9b008cce9a5433ae4c36c3418eba392e2c
EL HANANI Anasse <elhananianasse@ELs-Mac.local>
2016-11-12 11:53:21 +0100

    Grace file

--- Grace.c
+++ Grace.c
@@ -4,5 +4,5 @@
 */
 #define Nintendo
 #define VS
-#define FT(x) int main(void){FILE*p=fopen("Grace_kid.c", "w");char*c="#include <stdio.h>%c/*%c%ccommentaire inutile%c*/%c#define Nintendo%c#define VS%c#define FT(x) int main(void){FILE*p=fopen(%cGrace_kid.c%c, %cw%c);char*c=%c%s%c;fprintf(p,c,10,10,9,10,10,10,10,34,34,34,34,34,c,34,10,10);}%cFT(42)%c";fprintf(p,c,10,10,9,10,10,10,10,34,34,34,34,34,c,34,10,10);}
+#define FT(x) int main(void){FILE*p=fopen("Grace_kid.c", "w");if(!p)return(-1);char*c="#include <stdio.h>%c/*%c%ccommentaire inutile%c*/%c#define Nintendo%c#define VS%c#define FT(x) int main(void){FILE*p=fopen(%cGrace_kid.c%c, %cw%c);if(!p)return(-1);char*c=%c%s%c;fprintf(p,c,10,10,9,10,10,10,10,34,34,34,34,34,c,34,10,10);}%cFT(42)%c";fprintf(p,c,10,10,9,10,10,10,10,34,34,34,34,34,c,34,10,10);}
 FT(42)
